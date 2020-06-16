3a47356182d51d7e257d3438f7dc5335358cadf3
Adem ilter <ademilter@gmail.com>
2020-06-16 10:22:23 +0300

    pagination dışa çıkartıldı

--- components/news.js
+++ components/news.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import cn from 'classnames'
-import Link from 'next/link'
 
 import styles from './news.module.css'
+
 import NewsCard from './news-card'
-import Pagination from './pagination'
 
 function News({ data, totalData, className }) {
   return (
-    <>
-      <div className={cn(styles.news, className)}>
-        {data.map((article) => (
-          <NewsCard key={article.id} id={article.id} {...article.fields} />
-        ))}
-      </div>
-
-      {totalData && (
-        <Pagination className={styles.pagination} totalData={totalData}>
-          <Link href="/">
-            <a>1</a>
-          </Link>
-        </Pagination>
-      )}
-    </>
+    <div className={cn(styles.news, className)}>
+      {data.map((article) => (
+        <NewsCard key={article.id} id={article.id} {...article.fields} />
+      ))}
+    </div>
   )
 }
 
--- components/page.js
+++ components/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 import styles from './page.module.css'
 
@@ -6,13 +7,23 @@ import Title from './title'
 import Nav from './nav'
 import News from './news'
 import Layout from './layout'
+import Pagination from './pagination'
 
 function Page({ pathname, data, totalData }) {
   return (
     <Layout>
       <Title />
       <Nav className={styles.nav} location={pathname} />
+
       <News className={styles.news} data={data} totalData={totalData} />
+
+      {totalData && (
+        <Pagination className={styles.pagination} totalData={totalData}>
+          <Link href="/">
+            <a>1</a>
+          </Link>
+        </Pagination>
+      )}
     </Layout>
   )
 }
