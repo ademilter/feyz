4ae02e8242ff9d3b4d01145e191d292f4fad89f3
oktay <oktaycolakoglu@gmail.com>
2020-06-18 18:53:00 +0300

    #1: Tag pages fix

--- components/pagination.js
+++ components/pagination.js
@@ -9,12 +9,12 @@ import { pageFill } from '../utils/helper'
 function News({ totalData, className, tag }) {
   const PAGES = pageFill(totalData)
   const Router = useRouter();
-  
+
   return (
     <div className={cn(styles.pagination, className)}>
       {tag ? (
         <Link href={`/tag/[${tag}]`} as={`/tag/${tag}`}>
-          <a className={Router.asPath === `/tag/[${tag}]` ? styles.active : ''}>1</a>
+          <a className={Router.asPath === `/tag/${tag}` ? styles.active : ''}>1</a>
         </Link>
       ) : (
         <Link href="/">
@@ -28,7 +28,7 @@ function News({ totalData, className, tag }) {
             href={`/tag/[${tag}]/[page]`}
             as={`/tag/${tag}/${page}`}
           >
-            <a className={Router.asPath === `/tag/[${tag}]/${page}` ? styles.active : ''}>{page}</a>
+            <a className={Router.asPath === `/tag/${tag}/${page}` ? styles.active : ''}>{page}</a>
           </Link>
         ) : (
           <Link key={page} href="/[page]" as={`/${page}`}>
