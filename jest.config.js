5e9356a109bc312a38d3dfde3dede883b0c39809
Fatih <isafatihyildirim@gmail.com>
2020-06-23 20:03:55 +0300

    add jest-transform-stub for handling assets

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   transformIgnorePatterns: ['node_modules/(?!(imask)/)'],
   testURL: 'https://feyz.li',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub'
+  }
 }
